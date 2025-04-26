531f52a4af3887a3953598ad6ab99ee77c77aa4e
dandelionblown <dandelionblown143@gmail.com>
2025-04-26 13:40:02 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAPgsEkc0aSNiUhzUrGtmWlRcoi9-evpVU",
-  authDomain: "bestfriends-chat-bcb3f.firebaseapp.com",
-  projectId: "bestfriends-chat-bcb3f",
-  storageBucket: "bestfriends-chat-bcb3f.firebasestorage.app",
-  messagingSenderId: "693210748353",
-  appId: "1:693210748353:web:c97625018794823ddc0b40",
-  measurementId: "G-DPVDM67W91"
+  apiKey: "your-api-key",
+  authDomain: "your-project-id.firebaseapp.com",
+  projectId: "your-project-id",
+  storageBucket: "your-project-id.appspot.com",
+  messagingSenderId: "some-id",
+  appId: "some-app-id"
 };
+
+firebase.initializeApp(firebaseConfig);
